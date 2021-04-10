0a51ecd2feb36ffcc7af6d547be22a059387604e
cong tin <pct.hcmut@gmail.com>
2021-04-10 14:53:51 +0700

    làm quen tổ chức component TrangChu, trong TrangChu có Component Header, Content, SideBar, Footter

--- app.module.ts
+++ app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { FootterComponent } from './footter/footter.component';
+import { TrangChuComponent } from './trang-chu/trang-chu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
+    HeaderComponent,
+    ContentComponent,
+    SideBarComponent,
+    FootterComponent,
+    TrangChuComponent,
    
     
   ],
